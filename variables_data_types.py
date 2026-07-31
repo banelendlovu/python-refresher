e054dc3940cc70ce3bfdefc90da260284de090e5
banelendlovu <banelendlovu732@gmail.com>
2026-07-31 02:56:19 -0400

    Creating a user profile

---
+++ variables_data_types.py
@@ -0,0 +1,51 @@
+# VARIABLES & DATA TYPES
+
+# Exercise 1: Creating a profile
+
+name = 'Jane'
+age = 27
+country = 'USA'
+is_learning_python = True
+weekly_study_hrs = 14
+
+technical_skills = ['python', 'sql', 'r']
+
+degrees = ("bachelor's", "Master's", 'PhD')
+
+colors = {'red', 'blue', 'green'}
+
+data = {'occupation': 'founder',
+'siblings': 4,
+'college': 'William & Mary'}
+
+future_job = None
+
+print(name)
+print(type(name))
+
+print(age)
+print(type(age))
+
+print(country)
+print(type(country))
+
+print(is_learning_python)
+print(type(is_learning_python))
+
+print(weekly_study_hrs)
+print(type(weekly_study_hrs))
+
+print(technical_skills)
+print(type(technical_skills))
+
+print(degrees)
+print(type(degrees))
+
+print(colors)
+print(type(colors))
+
+print(data)
+print(type(data))
+
+print(future_job)
+print(type(future_job))
